79c30d4665ade162fd9649d1ce297e4c05af33d4
Vladislava Scherbo <vladislava.volokhovich@gmail.com>
2025-09-18 15:51:29 +0300

    Initial Commit

---
+++ AnagramsGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnagramsGame
+//
+//  Created by Vladislava Scherbo on 18.09.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
